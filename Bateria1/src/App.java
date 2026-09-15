7395bfc678dc203d12d91340a1965eb4af04e4b9
ariadnarc <ariadnaruizcastillo@gmail.com>
2026-09-15 19:57:12 +0200

    mammamia

--- Bateria1/src/App.java
+++ Bateria1/src/App.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 
 public class App {
+
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-       
+        
         /*
         ejercicio1();
         int ladoCuadrado = 5;
@@ -24,40 +27,34 @@ public class App {
         ejercicio16();
         ejercicio17();
         ejercicio18();
-       
+        
         */
 
+        sc.close();
     }
-   
+
     //--- BATERIA EJERCICIOS INTRODUCCION A JAVA ---
-   
+    
     public static void ejercicio1(){
         System.out.println("Buenos días!");
     }
-   
+    
     public static void ejercicio2(int lado){
         int area = multiplicacion(lado, lado);
         System.out.println("El área del cuadrado de lado " + lado + "cm es igual a " + area + "cm2.");
     }
-   
+    
     public static void ejercicio3(){
-        Scanner sc = new Scanner(System.in);
-
-
+        
         System.out.println("Introduzca la longitud en cm del lado del cuadrado: ");
         int lado = sc.nextInt();
 
 
         ejercicio2(lado);
 
-
-        sc.close();
     }
 
     public static void ejercicio4(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la primera cifra: ");
         int num1 = sc.nextInt();
         System.out.println("Introduzca la segunda cifra: ");
@@ -69,14 +66,9 @@ public class App {
         System.out.println("El producto de " + num1 + " x " + num2 + " es igual a " + multiplicacion(num1, num2) );
         System.out.println("La división de " + num1 + " entre " + num2 + " es igual a " + division(num1, num2) );
 
-
-        sc.close();
     }
 
     public static void ejercicio5(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la longitud del radio en cm: ");
         int radio = sc.nextInt();
 
@@ -85,14 +77,9 @@ public class App {
         System.out.println("El área del círculo es de " + areaCircunferencia(radio) + "cm2.");
         System.out.println("El volumen de la esfera es de " + volumenEsfera(radio) + "cm3.");
 
-
-        sc.close();
     }
 
     public static void ejercicio6(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca el precio al que se vende el artículo: ");
         float precioFinal = sc.nextFloat();
         System.out.println("Introduzca el precio original del artículo: ");
@@ -104,53 +91,29 @@ public class App {
 
 
         System.out.println("El artículo presenta un " + descuento + "% de descuento.");
-
-
-        sc.close();
     }
 
     public static void ejercicio7(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la distancia en millas marítimas: ");
         int millas = sc.nextInt();
         double metros = millas * 1.852;
         System.out.println("La distancia corresponde a " + metros + "metros.");
-
-
-        sc.close();
     }
 
     public static void ejercicio8(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca su edad: ");
         int edad = sc.nextInt();
         if(edad >= 18) System.out.println("Eres mayor de edad.");
-
-
-        sc.close();
     }
 
     public static void ejercicio9(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca su edad: ");
         int edad = sc.nextInt();
         if(edad >= 18) System.out.println("Eres mayor de edad.");
         else System.out.println("Eres menor de edad.");
-
-
-        sc.close();
     }
 
     public static void ejercicio10(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la primera cifra: ");
         int num1 = sc.nextInt();
         System.out.println("Introduzca la segunda cifra: ");
@@ -162,15 +125,9 @@ public class App {
         System.out.println("El producto de " + num1 + " x " + num2 + " es igual a " + multiplicacion(num1, num2) );
         if(num2 != 0) System.out.println("La división de " + num1 + " entre " + num2 + " es igual a " + division(num1, num2) );
         else System.out.println("No es posible realizar esta división.");
-
-
-        sc.close();
     }
 
     public static void ejercicio11(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la primera cifra: ");
         int num1 = sc.nextInt();
         System.out.println("Introduzca la segunda cifra: ");
@@ -179,29 +136,17 @@ public class App {
 
         if(num1 > num2) System.out.println(num1 + "es mayor que " + num2 );
         else System.out.println(num2 + " es mayor que " + num1 );
-
-
-        sc.close();
     }
 
     public static void ejercicio12(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la cifra: ");
         int num = sc.nextInt();
-       
+        
         if(num >= 0) System.out.println("Es positivo.");
         else System.out.println("Es negativo.");
-
-
-        sc.close();
     }
 
     public static void ejercicio13(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la primera cifra: ");
         int num1 = sc.nextInt();
         System.out.println("Introduzca la segunda cifra: ");
@@ -210,15 +155,9 @@ public class App {
 
         if(num1 > num2) System.out.println(num2 + ", " + num1 );
         else System.out.println(num1 + ", " + num2 );
-
-
-        sc.close();
     }
 
     public static void ejercicio14(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la primera cifra: ");
         int num1 = sc.nextInt();
         System.out.println("Introduzca la segunda cifra: ");
@@ -228,15 +167,9 @@ public class App {
         if(num1 > num2) System.out.println(num1 + " es mayor que " + num2 );
         else if (num1 < num2) System.out.println(num2 + " es mayor que " + num1 );
         else System.out.println("Las cifras son iguales.");
-
-
-        sc.close();
     }
 
     public static void ejercicio15(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la primera cifra: ");
         int num1 = sc.nextInt();
         System.out.println("Introduzca la segunda cifra: ");
@@ -248,15 +181,9 @@ public class App {
         if(num1 > num2 && num1 > num3) System.out.println(num1 + " es la cifra mayor.");
         else if (num2 > num1 && num2 > num3) System.out.println(num2 + " es la cifra mayor.");
         else System.out.println(num3 + " es la cifra mayor.");
-
-
-        sc.close();
     }
 
     public static void ejercicio16(){
-        Scanner sc = new Scanner(System.in);
-
-
         System.out.println("Introduzca la nota: ");
         double nota = sc.nextDouble();
 
@@ -267,13 +194,9 @@ public class App {
         else if (nota < 9) System.out.println("Notable.");
         else if (nota <= 10) System.out.println("Sobresaliente.");
         else System.out.println("La nota introducida no es válida.");
-
-        sc.close();
     }
 
     public static void ejercicio17(){
-        Scanner sc = new Scanner(System.in);
-
         System.out.print("Introduce las horas: ");
         int horas = sc.nextInt();
 
@@ -301,13 +224,9 @@ public class App {
 
         System.out.println("La hora dentro de un segundo será: "
                 + horas + ":" + minutos + ":" + segundos);
-
-        sc.close();
     }
 
     public static void ejercicio18(){
-        Scanner sc = new Scanner(System.in);
-
         System.out.print("Introduce el nombre: ");
         String nombre = sc.nextLine();
 
@@ -346,8 +265,6 @@ public class App {
         System.out.println("Salario bruto: " + salarioBruto + " €");
         System.out.println("Impuestos: " + impuestos + " €");
         System.out.println("Salario neto: " + salarioNeto + " €");
-
-        sc.close();
     }
 
 
@@ -379,6 +296,6 @@ public class App {
     public static double volumenEsfera(int radio){
         return (4.0 / 3.0) * Math.PI * Math.pow(radio, 3);
     }
-   
+
 
 }
